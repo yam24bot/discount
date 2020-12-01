69934fa9a87e13db4936c9218f9e00cf1cf333ed
Danil <dkdottk@gmail.com>
2020-12-01 18:55:38 +0200

    Create user mutation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,15 +1,9 @@
-# frozen_string_literal: true
+Types::QueryType = GraphQL::ObjectType.define do
+  name "Query"
 
-module Types
-  class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
-    def test_field
-      'Hello World!'
-    end
+  field :rentals, !types[Types::RentalType] do
+    resolve -> (obj, args, ctx) {
+      Rental.all
+    }
   end
 end
