8759462bf0711e2a5ccff233860f645b84981aac
Danil <dkdottk@gmail.com>
2020-12-02 13:21:42 +0200

    Configure Rubocop, SimpleCov, RSpec on Github CI
    Update  Ruby Version
    
    Update  Ruby Version
    
    Update  Ruby Version
    
    Update  Ruby Version
    
    Rspec simplecov update
    
    Rspec simplecov update
    
    Merge all CI`s
    
    Able to push
    
    Able to push
    
    Able to push
    
    Able to push
    
    Able to push
    
    Able to push
    
    Able to push

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,11 +2,11 @@ module Types
   class QueryType < BaseObject
     # queries are just represented as fields
     # `all_links` is automatically camelcased to `allLinks`
-    field :all_links, [LinkType], null: false
+    field :all_users, [UserType], null: false
 
     # this method is invoked, when `all_link` fields is being resolved
-    def all_links
-      Link.all
+    def all_users
+      User.all
     end
   end
 end
